183b09cf6e5bfc7c1546642ef14e25d04c9da6f7
Robert Skarżycki <robert.skarzycki@gmail.com>
2017-07-01 09:48:31 +0200

    First setup of projects with default HomeControllers

---
+++ Fibon.Api/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Api.Controllers
+{
+    public class HomeController : Controller{
+        
+        [HttpGet("")]
+        public IActionResult Get(){
+            return Content("Hello from FIBON API.");
+        }
+    }
+}
---
+++ Fibon.Service/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fibon.Service.Controllers
+{
+    
+    public class HomeController : Controller{
+        [HttpGet("")]
+        public IActionResult Get(){
+            return Content("Hello from FIBON Service.");
+        }
+    }
+}
